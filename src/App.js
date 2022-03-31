c0028aa545d349d89f9a668e9a47d774ec496fb4
Slava Ivshin <viacheslav.ivshin@genesys.com>
2022-03-31 16:44:05 +1000

    edit app.js + run prettier

--- src/App.js
+++ src/App.js
@@ -9,9 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>
-          Here is a new line
-        </p>
+        <p>Here is a new line</p>
 
         <a
           className="App-link"
