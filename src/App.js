a899b29b2b60a453dedcb4eb072978cd31e54cb6
Slava Ivshin <viacheslav.ivshin@genesys.com>
2022-03-31 16:37:22 +1000

    edit app.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Here is a new line
+        </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
