cbcab8aece18df474f9a8ac84375fbc504e77f5c
Slava Ivshin <viacheslav.ivshin@genesys.com>
2022-04-01 16:52:13 +1000

    feat: fixing file format

--- release.conf.js
+++ release.conf.js
@@ -7,10 +7,10 @@ module.exports = {
     "@semantic-release/npm",
     "@semantic-release/github",
     {
-        assets: [
-        { path: "build.zip", label: "Build"},
-        { path: "coverafe.zip", label: "Coverage"}
-    ]
-    }
+      assets: [
+        { path: "build.zip", label: "Build" },
+        { path: "coverafe.zip", label: "Coverage" },
+      ],
+    },
   ],
 };
