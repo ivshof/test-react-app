72e9f35c8a97ba5f07765bf889f6d113d6ac4b71
Slava Ivshin <viacheslav.ivshin@genesys.com>
2022-04-01 15:17:25 +1000

    fix: change steps sequence so upload build will happen after build fix the file struct

--- release.conf.js
+++ release.conf.js
@@ -1,6 +1,10 @@
 module.exports = {
-    branches: "master",
-    repositoryUrl: "https://github.com/ivshof/test-react-app"
-    plugins: ['@semantic-release/commit-analyzer', '@semantic-release/release-notes-generator', '@semantic-release/npm', '@semantic-release/github']
-
-}
+  branches: "master",
+  repositoryUrl: "https://github.com/ivshof/test-react-app",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/npm",
+    "@semantic-release/github",
+  ],
+};
